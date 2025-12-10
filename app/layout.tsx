f8702c46291e37ab303ff518a91e7b7afd57c694
Pranjal360Agarwal <pranjal360agarwal@gmail.com>
2025-12-11 00:34:21 +0530

    Project deployed

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,23 +13,6 @@ export const metadata: Metadata = {
   description:
     "Securely upload, view, and manage your medical documents including prescriptions, test results, and referral notes.",
   generator: "v.app",
-  icons: {
-    icon: [
-      {
-        url: "/icon-light-32x32.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/icon-dark-32x32.png",
-        media: "(prefers-color-scheme: dark)",
-      },
-      {
-        url: "/icon.svg",
-        type: "image/svg+xml",
-      },
-    ],
-    apple: "/apple-icon.png",
-  },
 };
 
 export const viewport: Viewport = {
